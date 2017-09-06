f6012d13a1172c49020626d9886bd4421a134ec2
ArbutusNate <arbutusnate@gmail.com>
2017-09-06 14:51:26 -0700

    Factored out some of the js, wrapped scroll functions in a document.ready, reordered script tags

--- js/javascript.js
+++ js/javascript.js
@@ -1,135 +1,119 @@
-const calculateAnimations = () => {
-  let winH = window.innerHeight;
-  let fullH = winH * 4;
-  return [
-  // animate rings //
-    { range: [-1, fullH],
-      selectors: ['#namebox-outer-ring'],
-      type: 'scale',
-      style: 'transform:rotateZ',
-      from: 0, to: -360,
-      unit: 'deg'
-    },
-    { range: [-1, fullH],
-      selectors: ['#namebox'],
-      type: 'scale',
-      style: 'transform:rotateZ',
-      from: 0, to: 980,
-      unit: 'deg'
-    },
-    { range: [-1, fullH],
-      selectors: ['.brand'],
-      type: 'scale',
-      style: 'transform:rotateZ',
-      //stationary name
-      from: 0, to: -620,
-      //for slow, downward rotation
-      // from: 0, to: -510,
-      unit: 'deg'
-    },
-    // Rotates the main Carousel
-    { range: [-1, fullH],
-      selectors: ['.carousel'],
-      type: 'scale',
-      style: 'transform:rotateZ',
-      from: 0, to: -360,
-      unit: 'deg'
-    },
-    // Counter-rotates the Content
-    { range: [-1, fullH],
-      selectors: ['.car-cont'],
-      type: 'scale',
-      style: 'transform:rotateZ',
-      from: 0, to: 360,
-      unit: 'deg'
-    },
-    // info-box
-    { range: [winH * .1, winH * .6],
-      selectors: ['#info-box'],
-      type: 'scale',
-      style: 'opacity',
-      from: 1, to: 0
-    },
-    { range: [winH * .1, winH * .6],
-      selectors: ['#info-box'],
-      type: 'scale',
-      style: 'transform:scaleX',
-      from: 1, to: .5
-    },
-    { range: [winH * .1, winH * .6],
-      selectors: ['#info-box'],
-      type: 'scale',
-      style: 'transform:scaleY',
-      from: 1, to: .5
-    },
-    // port-box
-    { range: [-1, winH * .7],
-      selectors: ['#port-box'],
-      type: 'scale',
-      style: 'opacity',
-      from: 0, to: 1
-    },
-    { range: [-1, winH * .7],
-      selectors: ['#port-box'],
-      type: 'scale',
-      style: 'transform:scaleX',
-      from: .5, to: 1
-    },
-    { range: [-1, winH * .7],
-      selectors: ['#port-box'],
-      type: 'scale',
-      style: 'transform:scaleY',
-      from: .5, to: 1
-    },
-    { range: [winH * .9, winH * 1.3],
-      selectors: ['#port-box'],
-      type: 'scale',
-      style: 'opacity',
-      from: 1, to: 0
-    },
-    { range: [winH * .9, winH * 1.3],
-      selectors: ['#port-box'],
-      type: 'scale',
-      style: 'transform:scaleX',
-      from: 1, to: 0
-    },
-    { range: [winH * .9, winH * 1.3],
-      selectors: ['#port-box'],
-      type: 'scale',
-      style: 'transform:scaleY',
-      from: 1, to: 0
-    },
-    // social-box
-    { range: [-1, winH * .8],
-      selectors: ['#social-box'],
-      type: 'scale',
-      style: 'opacity',
-      from: 0, to: 0
-    },
-    { range: [winH * .8, winH * 1.2],
-      selectors: ['#social-box'],
-      type: 'scale',
-      style: 'opacity',
-      from: 0, to: 1
-    },
-        { range: [winH * .71, winH * 1.2],
-      selectors: ['#social-box'],
-      type: 'scale',
-      style: 'transform:scaleX',
-      from: .5, to: 1
-    },
-        { range: [winH * .71, winH * 1.2],
-      selectors: ['#social-box'],
-      type: 'scale',
-      style: 'transform:scaleY',
-      from: .5, to: 1
-    },
-    { range: [winH * 1.4, winH * 1.7],
-      selectors: ['#social-box'],
-      type: 'scale',
-      style: 'opacity',
-      from: 1, to: 0
-    }
-  ]
+// DECLARE VARIABLES
+let pIndex = 0;
+// let win = $('window');
+let isScrolling = false;
+let clicked = false;
+let winOffset = window.pageYOffset;
+let pageIndex = [0, 450, 795, 1930];
+
+//Set Up Animationss
+let choreographer = new Choreographer({
+  //Imported from calculateAnimations.js
+  animations: calculateAnimations()
+})
+
+
+let animateScrolling = (destPage, optTimer) => {
+  console.log('scrolling to ' + destPage);
+  $('body').animate({ scrollTop: destPage }, optTimer || 2000, myCallback);
+};
+
+function myCallback() {
+  isScrolling = false;
+  console.log("isScrolling now set to False")
+  return
 }
 
+let scrollDecider = () => {
+  winOffset = window.pageYOffset;
+    // if(!isScrolling){
+      if(pIndex === 0 && winOffset > 3 && winOffset < 431) {
+        pIndex = 1;
+        isScrolling = true;
+        return animateScrolling(pageIndex[pIndex]);
+      }
+      if(pIndex === 1 && winOffset < pageIndex[pIndex]) {
+        pIndex = 0;
+        isScrolling = true;
+        return animateScrolling(pageIndex[pIndex]);
+      }
+      if(pIndex === 1 && winOffset > pageIndex[pIndex]) {
+        pIndex = 2;
+        isScrolling = true;
+        return animateScrolling(pageIndex[pIndex]);
+      }
+      if(pIndex === 2 && winOffset < pageIndex[pIndex]) {
+        pIndex = 1;
+        isScrolling = true;
+        return animateScrolling(pageIndex[pIndex]);
+      }
+      if(pIndex === 2 && winOffset > pageIndex[pIndex]) {
+        pIndex = 3;
+        isScrolling = true;
+        return animateScrolling(pageIndex[pIndex]);
+      }
+      if(pIndex === 3 && winOffset < pageIndex[pIndex]) {
+        pIndex = 2;
+        isScrolling = true;
+        return animateScrolling(pageIndex[pIndex]);
+      }
+    // } else {
+    //   console.log("still scrolling...")
+    // }
+}
+
+// function throttle(fn, wait) {
+//   var time = Date.now();
+//   return function() {
+//     if ((time + wait - Date.now()) < 0) {
+//       fn();
+//       time = Date.now();
+//     }
+//   }
+// }
+
+// window.addEventListener('scroll', throttle(scrollDecider, 2100));
+$(document).ready(function(){
+  window.addEventListener('scroll', function() {
+    choreographer.runAnimationsAt(window.pageYOffset);
+    if(!isScrolling && !clicked){
+      console.log("scrollDecider running, isScrolling set to true.");
+      return scrollDecider();
+    }
+  });
+});
+
+
+
+
+// $('.brand-text').on("click", () => {
+//   console.log("clicked");
+//   pIndex = 0;
+//   setTimeout(isScrolling = true, 2100)
+//   return animateScrolling(pageIndex[pIndex], 2100);
+// })
+
+$('#nav-info').on("click", () => {
+  pIndex = 0;
+  clicked = true;
+  isScrolling = true;
+  setTimeout(afterTimeout, 2200)
+  return animateScrolling(pageIndex[pIndex], 3000);
+})
+
+$('#nav-port').on("click", () => {
+  pIndex = 1;
+  isScrolling = true;
+  setTimeout(afterTimeout, 2200)
+  return animateScrolling(450, 3000);
+})
+
+$('#nav-cont').on("click", () => {
+  pIndex = 2;
+  setTimeout(afterTimeout, 2200)
+  return animateScrolling(pageIndex[pIndex], 3000);
+})
+
+function afterTimeout(){
+  return clicked = false;
+}
